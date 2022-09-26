86b6b6f05b4222ffea7a6092a2b084cf484d333f
Akshay Gosavi <112954638+akkisavi@users.noreply.github.com>
2022-09-26 15:54:58 +0530

    Add files via upload

---
+++ Cricketer.java
@@ -0,0 +1,14 @@
+package Assignment_practice.inital;
+
+public class Cricketer extends Player
+{
+	void bat(int r)
+	{
+		System.out.println("Runs made in current match: "+r);
+	}
+	
+	void ball(int w)
+	{
+		System.out.println("Wickets took in current match: "+w);
+	}
+}
