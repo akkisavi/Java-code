0976dd6884a84112b2a7fb84aec4067170f0566b
Akshay Gosavi <112954638+akkisavi@users.noreply.github.com>
2022-09-27 16:19:25 +0530

    Update Phone_Main.java

--- PhoneModel/Phone_Main.java
+++ PhoneModel/Phone_Main.java
@@ -4,16 +4,18 @@ public class Phone_Main {
 
 	public static void main(String[] args) 
 	{
-			
-		Realme r1 = new Realme();
-		r1.model();
-		r1.camera();
-		r1.storage();
+		Phone p1 = new Realme();
+
+		p1.model();
+		p1.camera();
+		p1.storage();
 		
-		IPhone i1 = new IPhone();
-		i1.model();
-		i1.camera();
-		i1.storage();
+		p1 = new IPhone();
+		
+		p1.model();
+		p1.camera();
+		p1.storage();
 	
     }
 }
+
